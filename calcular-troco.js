c3440552de27b5b1d481c5549da8cbd024f81dcd
Silmara Esteves <silmara.araujo@al.infnet.edu.br>
2024-11-21 13:39:05 -0400

    Calcular troco de um produto

--- calcular-troco.js
+++ calcular-troco.js
@@ -4,6 +4,6 @@ let valorPago = Number(prompt("Digite o valor pago em R$:"));
 if (valorProduto < 0 || valorPago < 0) {
   alert("Você não informou um número válido.");
 } else {
-  let troco = Math.floor(valorProduto - valorPago);
+  let troco = Math.round(valorPago - valorProduto);
   alert(`O troco será de R$${troco}`);
 }
